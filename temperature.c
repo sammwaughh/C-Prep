b58631ee7f1e5f3cb2a74736208030c59f9c245e
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-22 19:38:25 +0100

    reformatted

--- temperature.c
+++ temperature.c
@@ -5,10 +5,8 @@
 int main() {
     char* title = "Converter\n";
     printf("%13s", title);
-
     for (int fahr = 300; fahr >= 0; fahr -= 20) {
         printf("%3d\t%4.f\n", fahr, (5.0/9.0)*(fahr-32.0));
     }
-
     return 0;
 }
