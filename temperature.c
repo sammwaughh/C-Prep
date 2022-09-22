12bdb1347adc20a92d847693c4b8b9801a8c03bb
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-22 19:44:30 +0100

    used symbolic constants

--- temperature.c
+++ temperature.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 30
 /* Temperature conversion program */
 
 int main() {
     char* title = "Converter\n";
     printf("%13s", title);
-    for (int fahr = 300; fahr >= 0; fahr -= 20) {
+    for (int fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
         printf("%3d\t%4.f\n", fahr, (5.0/9.0)*(fahr-32.0));
     }
     return 0;
