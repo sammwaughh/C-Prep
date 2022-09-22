3f3fc0481441fcfa941729003fb76445581899e0
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-22 16:02:17 +0100

    completed exercise 1-4

--- temperature.c
+++ temperature.c
@@ -3,7 +3,22 @@
 /* Temperature conversion program */
 
 int main() {
-    char[] title = "Converter";
-    printf("%9.s", title);
+    char* title = "Converter\n";
+    printf("%13s", title);
+
+    float fahr, celcius;
+    int start, end, step;
+
+    start = 0;
+    end = 300;
+    step = 20;
+    celcius = start;
+
+    while (celcius <= end) {
+        fahr = (9.0/5.0) * celcius + 32.0;
+        printf("%3.0f\t%5.1f\n", celcius, fahr);
+        celcius += step;
+    }
+
     return 0;
 }
