d1b29fd854f0fe7467efe30e4deaed6b71268323
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-23 15:27:32 +0100

    print length arbitrarily long line despite max

--- ex-1-16.c
+++ ex-1-16.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
+#define MAXLINE 20
+
+int getline2(char line[], int maxline);
+void copy(char to[], char from[]);
 
 int main() {
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    max = 0;
+    while ((len = getline2(line, MAXLINE)) > 0) {
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    }
+    if (max > 0) {
+        printf("The longest line contains %d\n", max);
+        printf("Longest line: %s\n", longest);
+    }
     
     return 0;
+}
+
+int getline2(char s[], int lim) {
+    int c, i, j;
+    for (i = j = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+        if (i < lim) {
+            s[j++] = c;
+        }    
+    }
+    if (c == '\n') {
+        if (i < lim) {
+            s[j++] = c;
+        }
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[]) {
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0') {
+        ++i;
+    }
 }
