5e44f58227ef123728ebfe6a2102763b76a4c7f4
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-23 16:13:39 +0100

    altered program to ignore blank lines

--- ex-1-18.c
+++ ex-1-18.c
@@ -2,20 +2,15 @@
 #define MAXLINE 40
 
 int getline2(char line[], int maxline);
+int remove2(char s[]);
 
 int main() {
     int len;
     char line[MAXLINE];
-    while ((len = getline2(line, MAXLINE)) > 0) {
-        int i = len-2;
-        char end = line[i];
-        while (end == ' ' || end == '\t') {
-            --i;
-            end = line[i];
-        }
-        line[i+1] = '\n';
-        line[i+2] = '\0';
-        printf("%s\n", line);
+    while (getline2(line, MAXLINE) > 0) {
+        if (remove2(line) > 0) {
+            printf("%s\n", line);
+        }    
     }
     return 0;
 }
@@ -34,5 +29,24 @@ int getline2(char s[], int lim) {
         ++i;
     }
     s[i] = '\0';
+    return i;
+}
+
+int remove2(char s[]) {
+    int i;
+    i = 0;
+    while (s[i] != '\n') {
+        ++i;
+    }
+    --i;
+    while (i >= 0 && (s[i] == ' ' || s[i] == '\t')) {
+        --i;
+    }
+    if (i >= 0) {
+        ++i;
+        s[i] = '\n';
+        ++i;
+        s[i] = '\0';
+    }
     return i;
 }
