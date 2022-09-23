877e4b7f9631bcafe4b02cd542d57aed81a680c0
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-23 15:37:44 +0100

    started ex1-18

---
+++ ex-1-18.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define LIMIT 20
+#define MAXLINE 100
+
+int getline2(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main() {
+    int len;
+    char line[MAXLINE];
+    while ((len = getline2(line, MAXLINE)) > 0) {
+        if (len > LIMIT) {
+            printf("%s\n", line);
+        }
+    }
+    return 0;
+}
+
+int getline2(char s[], int lim) {
+    int c, i, j;
+    for (i = j = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+        if (i < lim) {
+            s[j++] = c;
+        }    
+    }
+    if (c == '\n') {
+        if (i < lim) {
+            s[j++] = c;
+        }
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[]) {
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0') {
+        ++i;
+    }
+}
