33f23819c3923f61765e87e8ac84ff439e449ecf
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-24 22:46:41 +0100

    corrected solution to terminate properly using a while loop

--- ex-2-2.c
+++ ex-2-2.c
@@ -13,11 +13,26 @@ int main() {
 
 int getline2(char s[], int lim) {
     int c, i, j;
-    for (i = j = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+    /*for (i = j = 0; (c = getchar()) != EOF && c != '\n'; i++) {
         if (i < lim) {
             s[j++] = c;
         }    
+    }*/
+    int loop = 1;
+    int i = 0;
+    while (loop) {
+        char c = getchar();
+
+        if (i < lim-1) {
+            loop = 0;
+        } else if (c == '\n') {
+            loop = 0;
+        } else if (c == EOF) {
+            loop = 0;
+        }
+        s[i++] = c;
     }
+
     for (i = 0; i < lim-1; ++i) {
         if (c=getchar() != '\n') {
             if (c != EOF) {
