a999abbead7ac24296ea39df60bc85628abbe26f
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-27 16:12:28 +0100

    finished bitcount

--- ex-2-9.c
+++ ex-2-9.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
 int bitcount(unsigned x);
+int oldBitcount(unsigned x);
 
 int main() {
-    
+    int n = 13;
+    printf("Number: %u\nOld Bitcount: %d\nNew Bitcount: %d\n", n, oldBitcount(n), bitcount(n));
     return 0;
 }
 
+int oldBitcount(unsigned x) {
+    int b;
+    for (b = 0; x != 0; x >>= 1) {
+        if (x & 01) {
+            b++;
+        }
+    }
+    return b;
+}
+
 int bitcount(unsigned x) {
-    
+    int b = 0;
+    while (x != 0) {
+        x &= (x-1);
+        b++;
+    }
+    return b;
 }
