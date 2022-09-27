c92ae36fc37a15a11a52eb8f0aee664ae7b9aed2
Samuel Waugh <samjmwaugh@gmail.com>
2022-09-27 16:18:43 +0100

    done new lower

--- ex-2-10.c
+++ ex-2-10.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int oldLower(int c);
-int lower(int c);
+char oldLower(char c);
+char lower(char c);
 
 int main() {
-
+    char c = 'B';
+    printf("%c %c %c\n", c, oldLower(c), lower(c));
     return 0;
 }
 
-int oldLower(int c) {
+char oldLower(char c) {
     if (c >= 'A' && c <= 'Z') {
         return c + 'a' - 'A';
     } else {
@@ -16,8 +17,8 @@ int oldLower(int c) {
     }
 }
 
-int lower(int c) {
-    
+char lower(char c) {
+    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
 }
 
 
