a415e66e432e9bae6e4faa3406d72b6da8cc8cb6
Samuel Waugh <samjmwaugh@gmail.com>
2022-10-05 17:12:52 +0100

    finished reverseEscape

--- ex-3-2.c
+++ ex-3-2.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 void escape(char s[], char t[]);
+void reverseEscape(char s[], char t[]);
 
 int main() {
-    char testString[] = "Hello\tWorld\n";
+    char testString[] = "Hello\\tWorld\\n";
+    printf("%s\n", testString);
     char testOutput[1000];
-    escape(testOutput, testString);
+    reverseEscape(testOutput, testString);
     printf("%s\n", testOutput);
     return 0;
 }
@@ -29,6 +31,34 @@ void escape(char s[], char t[]) {
                 s[j] = 't';
                 j++;
                 break;
+            default: 
+                s[j] = t[i];
+                j++;
+        }
+    }
+}
+
+void reverseEscape(char s[], char t[]) {
+    // Copying the string t to s but converting \n string and \t string 
+    // into single \n and \t characters so that they behave as such. Using a switch statement.
+
+    int i, j;
+    for (i = j = 0; t[i] != '\0'; ++i) {
+        switch (t[i]) {
+            case '\\':
+                switch(t[i+1]) {
+                    case 'n':
+                        s[j] = '\n';
+                        i++;
+                        j++;
+                        break;
+                    case 't':
+                        s[j] = '\t';
+                        i++;
+                        j++;
+                        break;
+                }
+                break; 
             default: 
                 s[j] = t[i];
                 j++;
