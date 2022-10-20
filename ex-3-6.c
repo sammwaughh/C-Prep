dfd9fcf78677c7b5bbc73e5edb9363745767bf97
Samuel Waugh <samjmwaugh@gmail.com>
2022-10-20 20:49:59 +0100

    added padding

--- ex-3-6.c
+++ ex-3-6.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-void itoa(int n, char s[]);
+void itoa(int n, char s[], int w);
 void reverse(char s[]);
 
 int main() {
     int n = 67;
     char s[1000];
-    itoa(n, s);
+    int w = 12;
+    itoa(n, s, w);
     printf("%s\n", s);
     return 0;
 }
 
-void itoa(int n, char s[]) {
-    int i, sign;
+void itoa(int n, char s[], int w) {
+    int i, j, sign;
     if ((sign = n) < 0) {
         n = -n;
     }
-    i = 0;
+    i = j = 0;
     do {
         s[i++] = n % 10 + '0';
-    } while (n /= 10 > 0);
+        j++;
+    } while ((n /= 10) > 0);
     if (sign < 0) {
         s[i++] = '-';
+        j++;
+    }
+    while (j < w) {
+        s[i++] = ' ';
+        j++;
     }
     s[i] = '\0';
     reverse(s);
